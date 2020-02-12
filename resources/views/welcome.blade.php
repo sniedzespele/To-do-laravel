6e47937d21f40013892918b47a242c4551deb54c
Sniedze Spele <spele.sniedze@gmail.com>
2020-02-13 01:32:17 +0200

    rename

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="panel1">
+                <div class="pheading"><h2 class="welcome">Welcome!</h2>
+            </div>
+
+                <div class="pbody">
+                Please login or register.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
